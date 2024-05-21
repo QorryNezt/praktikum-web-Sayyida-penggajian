c3478c1431d9635c66d46ebeaddeb7f2c7e2d413
qorrynezt <korivond@gmail.com>
2024-05-21 16:42:39 +0800

    2.1 Koneksi ke MySQL dan Persiapan Interface Table

--- pages/bagian.php
+++ pages/bagian.php
@@ -1,56 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="css/custom.css" />
-    <title>Aplikasi Penggajian</title>
-</head>
-
-<body>
-
+<?php
+include "database/connection.php";
+?>
 <div class="row">
                 <div class="col">
                     <h3>Bagian</h3>
                 </div>
+                <div class="col">
+                    <a href="#" class="btn btn-success float-end">
+                        <i class="fa fa-plus-circle"></i>
+                        Tambah
+                    </a>
+                </div>
 </div>
 <div class="row mt-3">
         <div class="col">
+                <?php
+                $selectSQL = "SELECT * FROM bagian";
+                $result = mysqli_query($connection, $selectSQL);
+                if (!$result) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo mysqli_error($connection); ?>
+                    </div>
+                <?php
+                    return;
+                }
+                if (mysqli_num_rows($result) == 0) {
+                ?>
+                    <div class="alert alert-light" role="alert">
+                        Data Kosong
+                    </div>
+                <?php
+                    return;
+                }
+                ?>
                 <table class="table bg-white rounded shadow-sm  table-hover">
                     <thead>
                             <tr>
                                 <th scope="col" width="50">#</th>
                                 <th scope="col">Bagian</th>
+                                <th scope="col" width="200">Opsi</th>
                             </tr>
                     </thead>
                         <tbody>
+                            <?php 
+                            $no = 1;
+                            while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
                             <tr class="align-middle">
-                                <th scope="row">1</th>
-                                <td>HRD</td>
+                                <td>
+                                    <?php echo $no++ ?>
+                                </td>
+                                <td>
+                                    <?php echo $row["nama"] ?>
+                                </td>
+                                <td>
+                                <a href="#" class="btn btn-primary">
+                                        <i class="fa fa-edit"></i>
+                                        Ubah
+                                    </a>
+                                    <a href="#" class="btn btn-danger">
+                                        <i class="fa fa-trash"></i>
+                                        Hapus
+                                    </a>
+                                </td>
                             </tr>
-                            <tr class="align-middle">
-                                <th scope="row">2</th>
-                                <td>Finance</td>
-                            </tr>
+                            <?php
+                            }
+                            ?>
                         </tbody>
             </table>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-    <script>
-        var el = document.getElementById("wrapper");
-        var toggleButton = document.getElementById("menu-toggle");
-
-        toggleButton.onclick = function () {
-            el.classList.toggle("toggled");
-        };
-    </script>
 </body>
-</html>
