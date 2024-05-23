d60ef5dda040d8d04dde18c49dbeac1cbcc719c7
qorrynezt <korivond@gmail.com>
2024-05-23 12:54:59 +0800

    2.2 Tambah Data Bagian

--- pages/bagiantambah.php
+++ pages/bagiantambah.php
@@ -9,33 +9,72 @@
         </a>
     </div>
 </div>
-<div id="pesan" class="row mb-3 col">
-    <div class="col">
+    <div id="pesan" class="row mb-3 col">
+        <div class="col">
         <?php
-        print_r($_POST);
-        ?>
+        include "database/connection.php";
+
+        if(isset($_POST["simpan_button"])){
+            $nama = $_POST["nama"];
+            
+            $sudahAda = false;
+            $checkSQL = "SELECT * FROM bagian WHERE nama='$nama'";
+            $resultCheck = mysqli_query($connection, $checkSQL);
+            if(mysqli_num_rows($resultCheck) > 0){
+                $sudahAda = true;
+            }
+
+            if ($sudahAda) {
+                ?>
+                <div class="alert alert-danger" role="alert">
+                    <i class="fa fa-exclamation-circle"></i>
+                    Nama bagian sudah ada
+                </div>
+            <?php
+            } else {
+            $insertSQL = "INSERT INTO bagian SET nama='$nama'";
+            $result = mysqli_query($connection, $insertSQL);
+            if (!$result) {
+            ?>
+                <div class="alert alert-danger" role="alert">
+                    <i class="fa fa-exclamation-circle"></i>
+                    <?php echo mysqli_error($connection) ?>
+                </div>
+                <?php
+                } else {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <i class="fa fa-check-circle"></i>
+                        Data berhasil disimpan
+                    </div>
+                <?php
+                }
+             } 
+        }
+        ?> 
+        </div>
     </div>
-</div>
+
 <div id="inputan" class="row mb-3 col">
     <div class="col">
         <div class="card px-3">
             <form action="" method="post">
-            <div class="mb-3">
-                <label for="nama" class="form-label">Nama Bagian</label>
-                <input type="text" class="form-control" id="nama" name="nama" placeholder="Misal : HRD" required>
-            </div>
-        <div class="col mb-3">
-            <button class="btn btn-success" type="submit" name="simpan_button">
-                <i class="fa fa-save"></i>
-                Simpan
-            </button>
-        </div>
+                <div class="mb-3">
+                    <label for="nama" class="form-label">Nama Bagian</label>
+                    <input type="text" class="form-control" id="nama" name="nama" placeholder="Misal : HRD" required>
+                </div>
+                <div class="col mb-3">
+                    <button class="btn btn-success" type="submit" name="simpan_button">
+                        <i class="fa fa-save"></i>
+                        Simpan
+                    </button>
+                </div>
             </form>
         </div>
     </div>
 </div>
 <script>
     if (window.history.replaceState) {
-        window.history.replaceState(null, null, window, location.href);
+        window.history.replaceState(null, null, window.location.href);
     }
 </script>
